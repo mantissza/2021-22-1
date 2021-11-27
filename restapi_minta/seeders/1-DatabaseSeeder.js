c970af860c3f6d32eaebdbbef9d4f981675b63a9
Tota Dávid <totadavid95@gmail.com>
2021-11-27 16:17:40 +0100

    REST API ZH mintamegoldás: Seeder

--- restapi_minta/seeders/1-DatabaseSeeder.js
+++ restapi_minta/seeders/1-DatabaseSeeder.js
@@ -4,13 +4,65 @@
 const faker = require("faker");
 const colors = require("colors");
 const models = require("../models");
-//const { ... } = models;
+const { Developer, Genre, Game, Release } = models;
 
 module.exports = {
     up: async (queryInterface, Sequelize) => {
         try {
             // Ide dolgozd ki a seeder tartalmát:
             // ...
+
+            const genres = [];
+            const genresCount = faker.datatype.number({ min: 5, max: 10 });
+            for (let i = 0; i < genresCount; i++) {
+                genres.push(
+                    await Genre.create({
+                        name: faker.unique(faker.lorem.word),
+                    })
+                );
+            }
+
+            const developers = [];
+            const developersCount = faker.datatype.number({ min: 5, max: 10 });
+            for (let i = 0; i < developersCount; i++) {
+                developers.push(
+                    await Developer.create({
+                        name: faker.company.companyName(),
+                        website: faker.internet.url(),
+                        location: faker.address.country(),
+                    })
+                );
+            }
+
+            const gamesCount = faker.datatype.number({ min: 5, max: 10 });
+            for (let i = 0; i < gamesCount; i++) {
+                const writers = []; // ['a', 'b', 'c'].join(', ') = "a, b, c"
+                const writersCount = faker.datatype.number({ min: 1, max: 3 });
+                for (let j = 0; j < writersCount; j++) {
+                    writers.push(faker.name.findName());
+                }
+                const game = await Game.create({
+                    title: faker.lorem.words(faker.datatype.number({ min: 1, max: 6 })),
+                    writers: writers.join(", "),
+                    description: faker.lorem.sentence(),
+                    singleplayer: faker.datatype.boolean(),
+                    multiplayer: faker.datatype.boolean(),
+                    engine: faker.lorem.word(),
+                    DeveloperId: faker.random.arrayElement(developers).id,
+                });
+                // Műfajok hozzárendelése a játékhoz
+                await game.setGenres(faker.random.arrayElements(genres));
+                // Kiadások hozzárendelése a játékhoz
+                const releasesCount = faker.datatype.number({ min: 2, max: 6 });
+                for (let j = 0; j < releasesCount; j++) {
+                    await game.createRelease({
+                        platform: faker.random.arrayElement(["win", "ps2", "ps3", "ps4", "ps5", "xbox360", "xboxone"]),
+                        date: faker.date.past(20, new Date()),
+                        version: faker.system.semver(),
+                        //GameId: game.id,
+                    });
+                }
+            }
 
             console.log("A DatabaseSeeder lefutott".green);
         } catch (e) {
