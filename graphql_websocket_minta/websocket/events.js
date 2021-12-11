3cb36b3159ce1f97958079df6522f5deee3681d7
Tota Dávid <totadavid95@gmail.com>
2021-12-11 12:37:02 +0100

    WS mintamegoldás

--- graphql_websocket_minta/websocket/events.js
+++ graphql_websocket_minta/websocket/events.js
@@ -1,50 +1,137 @@
 const faker = require("faker");
 const Joi = require("joi");
-//const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 const validate = require("./validate");
 
 // Socket.IO emit cheat sheet: https://socket.io/docs/v4/emit-cheatsheet/
 // Joi dokumentáció, példák: https://joi.dev/api/?v=17.5.0#general-usage
 
 let db = {
-    // ...
+    events: {
+        "esemény 1": {
+            uuid: "1111-2222-3333",
+            socketId: "teacherSocket1",
+            tasks: [
+                {
+                    uuid: "1111-2222-4444",
+                    description: "task leírása",
+                    solutions: [
+                        {
+                            solution: "megoldás szövege",
+                            evaluation: "értékelés szövege",
+                            socketId: "studentSocket1",
+                        },
+                    ],
+                },
+            ],
+        },
+    },
 };
 
 module.exports = {
     db,
     events: (io) => {
         io.on("connection", (socket) => {
-            // Ezeket töröld ki, mielőtt beadod a megoldást! Ez csak neked segítség:
-
-            // console.log("Client connected");
-            // console.log(socket);
-            // Socket események: socket.on("esemény neve", feldolgozó fv)
-            // Az első paraméter (data) egy JSON objektum, ami tartalmaz minden bejövő adatot, a második pedig az ack, a nyugtázó függvény
-            socket.on("test", (data, ack) => {
-                //console.log(data, ack);
-                socket.emit("test-client", "uzenet a szerverrol");
-                ack({
-                    status: "ok",
-                });
-            });
-
-            // Validate használata:
-            // socket.on("esemény neve", validate(validációk, feldolgozó fv))
             socket.on(
-                "number",
+                "create-event",
                 validate(
-                    // Validációk a data-ra vonatkozóan
                     Joi.object({
-                        // A data.number egy integer kell, hogy legyen
-                        number: Joi.number().integer().required(),
+                        name: Joi.string().trim().min(1).required(),
+                        tasks: Joi.array().items(
+                            Joi.object({
+                                description: Joi.string().trim().min(1).required(),
+                            })
+                        ),
                     }),
                     // Feldolgozó fv
-                    ({ number }, ack) => {
-                        // Ha a validáció sikeres volt, visszaadjuk a nyugtázást, egyébként a validator wrapper generál egy errort és azt küldi vissza a kliensnek
+                    ({ name, tasks }, ack) => {
+                        const uuid = uuidv4();
+                        db.events[name] = {
+                            uuid, // uuid: uuid,
+                            socketId: socket.id,
+                            tasks: tasks.map((task) => ({ ...task, uuid: uuidv4(), solutions: [] })),
+                        };
+                        console.log(JSON.stringify(db.events, null, 4));
                         ack({
-                            number,
                             status: "ok",
+                            uuid,
                         });
+                    }
+                )
+            );
+
+            socket.on(
+                "join-event",
+                validate(
+                    Joi.object({
+                        uuid: Joi.string().guid({
+                            version: ["uuidv4"],
+                        }),
+                    }),
+                    // Feldolgozó fv
+                    ({ uuid }, ack) => {
+                        let event = null;
+                        for (const [name, details] of Object.entries(db.events)) {
+                            if (details.uuid === uuid) {
+                                event = { name, ...details };
+                                break;
+                            }
+                        }
+                        if (!event) throw new Error("Event not found!");
+                        ack({
+                            status: "ok",
+                            event: {
+                                name: event.name,
+                                uuid: event.uuid,
+                                socketId: event.socketId,
+                            },
+                            tasks: event.tasks,
+                        });
+                    }
+                )
+            );
+
+            socket.on(
+                "send-solution",
+                validate(
+                    Joi.object({
+                        taskUUID: Joi.string().guid({
+                            version: ["uuidv4"],
+                        }),
+                        solution: Joi.string().trim().min(1).required(),
+                    }),
+                    // Feldolgozó fv
+                    ({ taskUUID, solution }, ack) => {
+                        let found = false;
+                        const solutionEntry = {
+                            // Megoldás szövege
+                            solution,
+                            // Kezdetben nincs értékelés
+                            evaluation: "",
+                            // Aki beküldte
+                            socketId: socket.id,
+                        };
+                        for (const [name, details] of Object.entries(db.events)) {
+                            for (task of details.tasks) {
+                                if (task.uuid === taskUUID) {
+                                    task.solutions.push(solutionEntry);
+                                    // Tanár értesítése
+                                    io.to(details.socketId).emit("solution-sent", {
+                                        event: name,
+                                        task: task.description,
+                                        solution: solutionEntry,
+                                    });
+                                    // Nyugtázás a hallgatónak:
+                                    ack({
+                                        status: "ok",
+                                    });
+                                    found = true;
+                                    break;
+                                }
+                            }
+                        }
+                        console.log(JSON.stringify(db.events, null, 4));
+                        if (!found) throw new Error("Task not found!");
                     }
                 )
             );
